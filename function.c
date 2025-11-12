70920a121ef2333ec065efa475269f0f884200cd
grilledcass <145569016+grilledcass@users.noreply.github.com>
2025-11-12 13:38:18 -0500

    :P

---
+++ function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function.h"
+
+
+playerMove(char board[MAX_SIZE][MAX_SIZE], int size, char player) {
+
+}
+
+aiMove(char board[MAX_SIZE][MAX_SIZE], int size){
+
+}
+
+checkWin(char board[MAX_SIZE][MAX_SIZE], int size){
+
+}
+
+checkDraw(char board[MAX_SIZE][MAX_SIZE], int size){
+
+}
+
+updateScore(char winner){
+    
+}

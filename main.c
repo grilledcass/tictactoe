67eae84fae0b0996003814f03a5d18ea03358798
kuoxoxo <72881007+kuoxoxo@users.noreply.github.com>
2025-11-12 13:14:05 -0500

    basic logic main

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MAXSIZE 10 
+
 int main(){
-    char board[9] = {"?"};
-    
+    //Initializing boardsize
+    int boardSize = 0;
+    int playWithAI = 0;
+    while (boardSize < 3 || boardSize > MAXSIZE){
+        printf("Enter the board size (max %d): ", MAXSIZE);
+        scanf("%d", &boardSize);
+    }
+    char board[boardSize][boardSize];
 
+    //Asking if user wants to play with AI
+    do{
+        printf("Do you want to play with AI? (1 for Yes, 0 for No): ");
+        scanf("%d", &playWithAI);
+    } while (playWithAI != 1 && playWithAI != 0);
+
+    return 0;
 }
 
-int printBoard(){
-    
-}
 
-int winCheck(){
-
-}
-
-int userPlay(){
-
-}
-
-int aiMode(){
-
-}
 
